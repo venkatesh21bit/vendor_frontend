4f87798f5f23b61236752c9697fb5e5e02a5d849
venkatesh21bit <venkatesh.k21062005@gmail.com>
2025-06-29 21:18:42 +0530

    hard code removed manufacturer page

--- app/manufacturer/page.tsx
+++ app/manufacturer/page.tsx
@@ -184,38 +184,7 @@ type Payment = {
   email: string;
 };
 
-export const payments: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-  {
-    id: "489e1d42",
-    amount: 125,
-    status: "processing",
-    email: "example@gmail.com",
-  },
-  {
-    id: "489e1d4552",
-    amount: 125,
-    status: "success",
-    email: "example@gmail.com",
-  },
-  {
-    id: "489e1d432",
-    amount: 125,
-    status: "failed",
-    email: "example@gmail.com",
-  },
-  {
-    id: "489e1d422",
-    amount: 125,
-    status: "failed",
-    email: "example@gmail.com",
-  },
-];
+
 
 // MQTT Topics
 const ANOMALY_TOPIC = "manufacturing/anomalies";
