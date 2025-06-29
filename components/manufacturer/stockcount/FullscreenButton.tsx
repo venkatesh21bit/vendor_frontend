0e33a89b483f456db1cf9c5c5b899391383254fc
venkatesh21bit <venkatesh.k21062005@gmail.com>
2025-06-29 21:24:35 +0530

    stockdata

--- components/manufacturer/stockcount/FullscreenButton.tsx
+++ components/manufacturer/stockcount/FullscreenButton.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import React, { useState } from 'react';
-import { stockData } from './data';
+import { useStockData } from './data';
 import SidePanel from './SidePanel';
 import NavigationBar from './NavigationBar';
 import StockOverview from './StockOverview';
 
 export default function StockCountPage() {
   const [activeView, setActiveView] = useState<string>('table');
+  const stockData = useStockData();
 
   return (
     <div className="relative p-6 bg-black text-blue-300 min-h-screen">
@@ -18,12 +19,12 @@ export default function StockCountPage() {
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         {/* Main Content - 2/3 width on large screens */}
         <div className="lg:col-span-2">
-          <StockOverview activeView={activeView} stockData={stockData} />
+          <StockOverview activeView={activeView} stockData={stockData.stockData} />
         </div>
         
         {/* Side Panel - 1/3 width on large screens */}
         <div className="space-y-6">
-          <SidePanel stockData={stockData} />
+          <SidePanel stockData={stockData.stockData} />
         </div>
       </div>
     </div>
